24f30a50136e44b5aa2e2b66efb2ed22a11dc3f9
AlexeyLVB <korolevaleksej12@gmail.com>
2022-11-03 15:44:00 +0300

    The MenuComponent component is written

--- src/app/Components/2. Tasks/by-task-number/by-task-number.component.ts
+++ src/app/Components/2. Tasks/by-task-number/by-task-number.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem, PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-by-task-number',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ByTaskNumberComponent implements OnInit {
 
+  items: MenuItem[] = [];
+  home!: MenuItem;
+
+  data: any;
+  chartOptions: any;
+
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.items = [
+      {
+        label: 'Тестовые задания'      
+      },
+      {
+        label: 'По номеру вопроса'      
+      }
+    ]
 
+    this.home = {icon: 'pi pi-home', routerLink: '/'};
+  
+  this.data = {
+    labels: ['Пройдено верно','Пройдено с ошибкой','Не пройдено'],
+    datasets: [
+        {
+            data: [650, 100, 50],
+            backgroundColor: [
+                "#77DD77",
+                "#F66257",
+                "#1E90FF"
+            ],
+            hoverBackgroundColor: [
+                "#66FF00",
+                "#FF0000",
+                "#00BFFF"
+            ]
+        }
+    ]
+};
+};
 }
