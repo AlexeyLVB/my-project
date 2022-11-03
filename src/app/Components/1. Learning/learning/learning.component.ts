d47007d71396bddf29b91a915f65b84b61d92700
AlexeyLVB <korolevaleksej12@gmail.com>
2022-11-03 15:33:38 +0300

    The LearningComponent component is written

--- src/app/Components/1. Learning/learning/learning.component.ts
+++ src/app/Components/1. Learning/learning/learning.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem, PrimeNGConfig } from 'primeng/api';
+
 
 @Component({
   selector: 'app-learning',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LearningComponent implements OnInit {
 
+  items: MenuItem[] = [];
+  home!: MenuItem;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Обучение'      
+      }
+    ]
+
+    this.home = {icon: 'pi pi-home', routerLink: '/'};
   }
 
 }
