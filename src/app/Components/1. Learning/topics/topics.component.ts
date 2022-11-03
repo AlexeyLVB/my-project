61a14a0d4a81469c013e451bc958f99a65c8dfd8
AlexeyLVB <korolevaleksej12@gmail.com>
2022-11-03 15:39:48 +0300

    The TopicsComponent component is written

--- src/app/Components/1. Learning/topics/topics.component.ts
+++ src/app/Components/1. Learning/topics/topics.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem, PrimeNGConfig } from 'primeng/api';
+
 
 @Component({
   selector: 'app-topics',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopicsComponent implements OnInit {
 
+  items: MenuItem[] = [];
+  home!: MenuItem;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Обучение',
+        routerLink: 'learning'     
+      },
+      {
+        label: 'Темы и разделы'
+      }
+    ]
+
+    this.home = {icon: 'pi pi-home', routerLink: '/'};
   }
 
 }
