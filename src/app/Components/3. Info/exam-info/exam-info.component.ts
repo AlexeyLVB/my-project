8d7f094de7ff141d9bed06e4a1c025bb2e9ab31c
AlexeyLVB <korolevaleksej12@gmail.com>
2022-11-03 15:51:37 +0300

    The ExamInfoComponent component is written

--- src/app/Components/3. Info/exam-info/exam-info.component.ts
+++ src/app/Components/3. Info/exam-info/exam-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem, PrimeNGConfig } from 'primeng/api';
+
 
 @Component({
   selector: 'app-exam-info',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExamInfoComponent implements OnInit {
 
+  items: MenuItem[] = [];
+  home!: MenuItem;
+
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.items = [
+      {
+        label: 'Полезная информация'      
+      },
+      {
+        label: 'Информация об экзамене'      
+      }
+    ]
 
+    this.home = {icon: 'pi pi-home', routerLink: '/'};
+  }
 }
