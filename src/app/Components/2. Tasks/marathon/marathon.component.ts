b75e013a38b80e1505163f1ad8d79f9f42a3e7a7
AlexeyLVB <korolevaleksej12@gmail.com>
2022-11-03 15:48:23 +0300

    The MarathonComponent component is written

--- src/app/Components/2. Tasks/marathon/marathon.component.ts
+++ src/app/Components/2. Tasks/marathon/marathon.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem, PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-marathon',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarathonComponent implements OnInit {
 
+  items: MenuItem[] = [];
+  home!: MenuItem;
+
+  data: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Тестовые задания'      
+      },
+      {
+        label: 'Марафон'      
+      }
+    ]
+    this.home = {icon: 'pi pi-home', routerLink: '/'};
+    this.data = {
+      labels: ['Пройдено верно','Пройдено с ошибкой','Не пройдено'],
+      datasets: [
+          {
+              data: [230, 100, 470],
+              backgroundColor: [
+                  "#77DD77",
+                  "#F66257",
+                  "#1E90FF"
+              ],
+              hoverBackgroundColor: [
+                  "#66FF00",
+                  "#FF0000",
+                  "#00BFFF"
+              ]
+          }
+      ]
+  };
   }
-
 }
